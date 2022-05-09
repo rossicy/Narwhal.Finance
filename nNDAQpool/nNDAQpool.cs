babaa62f88594332a7f2b1acca04bd47ec5aa855
UngroundableButters <40021166+UngroundableButters@users.noreply.github.com>
2022-05-09 17:29:24 +0800

    Add files via upload

--- nNDAQpool/nNDAQpool.cs
+++ nNDAQpool/nNDAQpool.cs
@@ -1,1 +1,82 @@
-test
+using System;
+using System.Numerics;
+using System.ComponentModel;
+using Neo;
+using Neo.SmartContract;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Native;
+using Neo.SmartContract.Framework.Services;
+
+
+
+
+namespace NarContracts.nNDAQpool
+{
+    [ManifestExtra("Author", "Nar")]
+    [SupportedStandards("NEP-17")]
+    [ContractPermission("*", "onNEP17Payment")]
+    public partial class NEP17Demo : Nep17Token
+    {
+        [InitialValue("NYehoujdJLiYcxSFaJdkRerFbNtcvf73R1", ContractParameterType.Hash160)]
+        private static readonly UInt160 owner = default;
+        // Prefix_TotalSupply = 0x00; Prefix_Balance = 0x01;
+        private const byte Prefix_Contract = 0x02;
+        public static readonly StorageMap ContractMap = new StorageMap(Storage.CurrentContext, Prefix_Contract);
+        private static readonly byte[] ownerKey = "owner".ToByteArray();
+        private static bool IsOwner() => Runtime.CheckWitness(GetOwner());
+        public override byte Decimals() => 2;
+        public override string Symbol() => "nNDAQpool";
+
+
+
+        public static void _deploy(object data, bool update)
+        {
+            if (update) return;
+            ContractMap.Put(ownerKey, owner);
+        }
+
+        public static UInt160 GetOwner()
+        {
+            return (UInt160)ContractMap.Get(ownerKey);
+        }
+
+        public static new void Mint(UInt160 account, BigInteger amount)
+        {
+            if (!IsOwner()) throw new InvalidOperationException("No Authorization!");
+            Nep17Token.Mint(account, amount);
+
+        }
+
+        public static new void Burn(UInt160 account, BigInteger amount)
+        {
+            if (!IsOwner()) throw new InvalidOperationException("No Authorization!");
+            Nep17Token.Burn(account, amount);
+        }
+
+        public static bool Update(ByteString nefFile, string manifest)
+        {
+            if (!IsOwner()) throw new InvalidOperationException("No Authorization!");
+            ContractManagement.Update(nefFile, manifest, null);
+            return true;
+        }
+
+        public static bool Destroy()
+        {
+            if (!IsOwner()) throw new InvalidOperationException("No Authorization!");
+            ContractManagement.Destroy();
+            return true;
+        }
+
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
